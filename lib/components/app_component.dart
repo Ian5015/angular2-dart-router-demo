3aea69d169cc7d14ec0717db9cad5cd4b1661e6b
Tim <ian5015@live.com>
2016-03-29 20:37:52 -0400

    minor dart format change

--- lib/components/app_component.dart
+++ lib/components/app_component.dart
@@ -6,9 +6,8 @@ import 'package:dart_sandbox/components/profile/profile_component.dart';
 
 @Component(selector: 'my-app')
 @View(templateUrl: 'app_component.html', directives: const [ROUTER_DIRECTIVES])
-@RouteConfig(
-    const [
-        const Route(path: '/home', name: 'Home', component: HomeComponent),
-        const Route(path: '/profile', name: 'Profile', component: ProfileComponent)
-    ])
+@RouteConfig(const [
+  const Route(path: '/home', name: 'Home', component: HomeComponent),
+  const Route(path: '/profile', name: 'Profile', component: ProfileComponent)
+])
 class AppComponent {}
